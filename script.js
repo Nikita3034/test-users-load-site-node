5c2dc9cbc2f3eef2df756e5fab41b727161d74e0
Nikita Turko <goodvinvangood@gmail.com>
2025-09-18 17:19:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 import http from 'k6/http'
 
 export const options = {
+	// maximum number of redirects
     maxRedirects: 10,
+	// load testing scenarios
     scenarios: {
+		// name of scenario
     	contracts: {
+			// type of scenario
+			// shared-iterations | per-vu-iterations | constant-vus | ramping-vus | constant-arrival-rate | ramping-arrival-rate
     		executor: 'constant-arrival-rate',
+			// number of requests per second
     		rate: 50,
+			// time of execution scenario
     		duration: '30s',
+			// number of virtual users (pre-selected before running test)
     		preAllocatedVUs: 50,
+			// number of virtual users (can be created during execution of test)
     		maxVUs: 100,
     	},
     },
